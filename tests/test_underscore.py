87df7c9ff6929e44928a43bd9333a9edbed963f3
Yuta Hinokuma <yuta@abeja.asia>
2015-05-24 18:57:47 +0900

    SFでdecorateした関数が_以外の引数を取れない取れない問題を見つけた

--- tests/test_underscore.py
+++ tests/test_underscore.py
@@ -47,15 +47,21 @@ class UnderscoreTest(TestCase):
     def test_scalambdable_func(self):
         def test(x):
             return 100 + x
+        def test2(x, y):
+            return x + y + 1
         eq_((SF(test)(10) + _)(1000), 1110)
         eq_(SF(len)(_)([1, 2, 3]), 3)
         eq_(SF(len)(_)(range(100)), 100)
         eq_((SF(len)(_) + 1)([range(1), range(2), range(3)]), 4)
         eq_(map((SF(len)(_) + 1), [range(1), range(2), range(3)]), [2, 3, 4])
+        eq_(SF(test2)(_, 2) + 1(100), 104)
 
     def test_readme(self):
         eq_(map(_ + 1, [1, 2, 3, 4]), [2, 3, 4, 5])
         eq_(filter(_.isdigit(), "ab123aad"), "123")
         eq_(reduce(_ + _, [1, 2, 3, 4]), 10)
         eq_(map(SF(len)(_) + 1, [[1], [1, 2], [1, 2, 3]]), [2, 3, 4])
-
+    
+    def test_high_stress(self):
+        for i in range(6):
+            eq_(map(_ + 10, xrange(10 ** i)), range(10, 10 ** i + 10))
