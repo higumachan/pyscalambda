5cfddfd2156904de607e92741537284152104f21
Yuta Hinokuma <yuta@abeja.asia>
2015-05-24 16:11:20 +0900

    versionを0.1.2 にする

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyscalambda',
-    version='0.1.1',
+    version='0.1.2',
     description='Use scala style lambda formula.',
     author='higumachan',
     author_email='harekumo792154@gmail.com',
