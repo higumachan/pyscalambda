fbb13ae994e11e6d7ccff0f1ad93f99950fa5449
Nao Yonashiro <owan.orisano@gmail.com>
2017-01-23 01:44:12 +0900

    引数が1つの時以外functools.wrapsを呼ばない様にした

--- pyscalambda/scalambdable.py
+++ pyscalambda/scalambdable.py
@@ -10,12 +10,11 @@ from pyscalambda.utility import convert_oprand, vmap
 def scalambdable_func(fn, *funcs):
     """
     :type fn: (T)->U
-    :type funcs: list[(Any)->Any]
+    :type funcs: ((Any)->Any, ...)
     :rtype: (T)->U
     """
-    @functools.wraps(fn)
-    def wraps(*args, **kwargs):
-        for f in reversed([fn] + funcs):
+    def wrapped(*args, **kwargs):
+        for f in reversed((fn,) + funcs):
             def is_scalambda_object(x):
                 return issubclass(x.__class__, Formula)
             if any(map(is_scalambda_object, args)) or any(map(is_scalambda_object, kwargs.values())):
@@ -25,4 +24,4 @@ def scalambdable_func(fn, *funcs):
                 args = [f(*args, **kwargs)]
                 kwargs = {}
         return args[0]
-    return wraps
+    return wrapped if funcs else functools.wraps(fn)(wrapped)
