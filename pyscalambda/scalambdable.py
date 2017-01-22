d71a9cb43410b09fdbdcebe679b6478d190a69d5
Nao Yonashiro <owan.orisano@gmail.com>
2017-01-23 00:26:38 +0900

    引数を1つ以上受け取るようにした

--- pyscalambda/scalambdable.py
+++ pyscalambda/scalambdable.py
@@ -7,10 +7,10 @@ from pyscalambda.formula_nodes import FunctionCall
 from pyscalambda.utility import convert_oprand, vmap
 
 
-def scalambdable_func(*funcs):
-    @functools.wraps(funcs[0])
+def scalambdable_func(fn, *funcs):
+    @functools.wraps(fn)
     def wraps(*args, **kwargs):
-        for f in reversed(funcs):
+        for f in reversed([fn] + funcs):
             def is_scalambda_object(x):
                 return issubclass(x.__class__, Formula)
             if any(map(is_scalambda_object, args)) or any(map(is_scalambda_object, kwargs.values())):
