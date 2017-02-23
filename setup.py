566e83185522d794f329dc142b920ddee224b304
Yuta Hinokuma <yuta@abeja.asia>
2017-02-24 00:45:47 +0900

    update MANIFEST

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 
 setup(
     name='pyscalambda',
-    version='0.3.0',
+    version='0.3.1',
     description='Can use scala style lambda formula.',
     author='higumachan',
     author_email='harekumo792154@gmail.com',
     license='MIT',
     url='https://github.com/higumachan/pyscalambda',
-    py_modules=["pyscalambda"]
+    #package_dir={'pyscalambda': 'pyscalambda/'}
+    packages=["pyscalambda"],
 )
