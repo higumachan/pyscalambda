aa24725858e5bb733713562f8fd24be212504c94
Yuta Hinokuma <yuta@abeja.asia>
2017-02-24 00:13:00 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='pyscalambda',
-    version='0.2.2',
+    version='0.3.0',
     description='Can use scala style lambda formula.',
     author='higumachan',
     author_email='harekumo792154@gmail.com',
